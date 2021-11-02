d4f0f813f12e5e92ff2d27ada91929054771ab33
Isaac Martinez Rizo <develop.martinezrizo@gmail.com>
2021-11-02 12:45:07 -0600

    agregando datos

---
+++ PARTE 06 DOM fundamentos/element.js
@@ -0,0 +1,6 @@
+const h1 = document.getElementById('tituloWeb');
+console.log(h1.className);
+h1.style.backgroundColor = "red";
+console.log(h1.attributes);
+console.log(h1.textContent);
+h1.textContent = "Nuevo texto desde JS 🎅";
